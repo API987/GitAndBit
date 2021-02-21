9b95b3651e7ce48c9bf5b1d5f2b4bb7201b1abc4
Ketaki <ketaki.gokhale@apisero.com>
2021-02-21 16:10:26 +0530

    1 file changed

--- Simple.java
+++ Simple.java
@@ -1,12 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Ketaki, this works");  
-	System.out.println("Hello Ketaki, this still works"); 
-	System.out.println("new change");
-	System.out.println("a local new change");
-	System.out.println("another local new change");
-	System.out.println("new change made from github");
-	System.out.println("new change from github 2");
-	System.out.println("new change from github 3");
+     System.out.println("This needs to be pushed to GitHub and Bit bucket together.");  
     }  
 } 
